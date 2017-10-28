fedde9ece2d0220ae25ded2a21258b95c45d0654
Nicolas Abatedaga Biole <nicolas.abatedaga@mercadolibre.com>
2017-10-28 19:15:45 -0300

    Reestructuro

---
+++ App/main.go
@@ -0,0 +1,13 @@
+package App
+
+import (
+	"fmt"
+	"huffman_hamming/hamming"
+	"huffman_hamming/huffman"
+)
+
+func main() {
+	fmt.Println("Primer impresion")
+	hamming.Hamming("parametro1")
+	huffman.Huffman("parametro2")
+}
