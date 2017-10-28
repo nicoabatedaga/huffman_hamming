faf659120eb9c0af756bba4419050577ac0d23bc
Nicolas Abatedaga Biole <nicolas.abatedaga@mercadolibre.com>
2017-10-28 19:06:12 -0300

    primer commit

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
-package main
+package huffman_hamming
 
 import (
+	"Hamming"
+	"Huffman"
 	"fmt"
 )
 
 func main() {
 	fmt.Println("Primer impresion")
+	Hamming.Hamming("parametro1")
+	Huffman.Huffman("parametro2")
 }
