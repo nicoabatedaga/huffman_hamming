2b070653351d8db79264dfff4eff07533999ac00
Nicolas Abatedaga Biole <nicolas.abatedaga@mercadolibre.com>
2017-10-28 23:37:39 -0300

    Leyendo archivo de forma concurrente con un waitGroup

--- huffman/huffman.go
+++ huffman/huffman.go
@@ -1,5 +1,29 @@
 package huffman
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sync"
+)
+
 func Huffman() {
+	fmt.Println("empiezo el huffman")
+	archivo, err := os.Open("./archivo")
+	if err != nil {
+		panic(fmt.Sprintf("hubo un error, %v", err))
+	}
+	scanner := bufio.NewScanner(archivo)
+	wg := new(sync.WaitGroup)
+	for scanner.Scan() {
+		wg.Add(1)
+		imprimirLinea(scanner.Text())
+		wg.Done()
+	}
+	wg.Wait()
 	return
+}
+
+func imprimirLinea(linea string) {
+	fmt.Println(linea)
 }
--- main.go
+++ main.go
@@ -2,17 +2,12 @@ package main
 
 import (
 	"fmt"
-	"huffman_hamming/hamming"
 	"huffman_hamming/huffman"
-	"time"
 )
 
 func main() {
-	s := time.Now().Nanosecond()
 	fmt.Println("Empiezo ejecucion")
-	hamming.Hamming()
+	// hamming.Hamming()
 	huffman.Huffman()
-	f := time.Now().Nanosecond()
-	attempt := f - s
-	fmt.Printf("tiempo transcurrido: %vns", attempt)
+	fmt.Println("Fin de la ejecucion")
 }
