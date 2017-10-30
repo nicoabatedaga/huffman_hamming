e57948119a7e82aec6fdd994bf7fcda0813e007c
Franco Joaquín Loyola <loyojoaquin@gmail.com>
2017-10-30 15:20:25 -0300

    Signaturas
    Se agregan las signaturas de la funciones para cifrar/descifrar

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -3,3 +3,44 @@ package hamming
 func Hamming() {
 	return
 }
+
+func getCodificacion(){
+
+}
+
+func esPotenciaDeDos(){
+
+}
+
+func bitsInformacion(){
+
+}
+
+func bitsParidad(){
+
+}
+
+func h(){
+
+}
+
+func g(){
+
+}
+
+func Codificar(){
+
+}
+
+func tieneError(){
+
+}
+
+func Decodificar(){
+
+}
+
+func AgregarError(){
+	
+}
+
