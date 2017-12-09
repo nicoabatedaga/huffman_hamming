eb47899f340437e89dccd2967c85042f33d9f5da
Franco Joaquín Loyola <loyojoaquin@gmail.com>
2017-12-09 17:57:59 -0300

    Cambios falta manejo de archivos

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -1,336 +1,272 @@
 package hamming
 
 import (
+	"bufio"
 	"fmt"
 	"math"
 	"math/rand"
-	"time"
 	"os"
-	"runtime/trace"
+	"time"
 )
 
 //Hamming es el programa de prueba
 func Hamming() {
-	f, err := os.Create("trace.out")
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-
-	err = trace.Start(f)
-	if err != nil {
-		panic(err)
-	}
-	defer trace.Stop()
-
-    start := time.Now()
+	start := time.Now()
 	fmt.Println("-Hamming-")
 
-	codificacion := 32
-	fmt.Println(fmt.Sprintf("Codificacion: %v, Bits Paridad: %v, Bits Información: %v", 
+	//codificacion := 522
+	//codificacion = 1035
+	codificacion := 2060
+
+	fmt.Println(fmt.Sprintf("Codificacion: %v, Bits Paridad: %v, Bits Información: %v",
 		codificacion,
 		bitsParidad(codificacion),
 		bitsInformacion(codificacion)))
-	//pruebaMatriz32()
-	pruebaHGR(128,false)
-	
-    elapsed := time.Since(start)
-    fmt.Println(fmt.Sprintf("Estuvo ejecutando: %s", elapsed))
+
+	//pruebaHGR(codificacion)
+	Proteger("prueba.txt", "prueba.ham", 522)
+	elapsed := time.Since(start)
+	fmt.Println(fmt.Sprintf("Estuvo ejecutando: %s", elapsed))
 	return
 }
 
-func pruebaMatriz32(){
-	ma := g(7)
-	ma32 := ma.MatrizTo32()
-	r32 := []uint32{5}
-	mb := MatrizFila(r32) 
-	error,m := ma32.Mult(mb)
-	if error{
-		fmt.Println("Error al multiplicar")
+func manejoError(err error) {
+	if err != nil {
+		panic(fmt.Sprintf("hubo un error, %v", err))
 	}
-	
-	fmt.Println("g:")
-	fmt.Println(ma32.ToString())
-	fmt.Println("Entrada:")
-	fmt.Println(mb.ToString())
-	fmt.Println("Matriz Resultante:")
-	fmt.Println(m.ToString())
-	/*
-	fmt.Println("g:")
-	fmt.Println(ma32.ToStringB(7))
-	fmt.Println("Entrada:")
-	fmt.Println(mb.ToStringB(7))
-	fmt.Println("Matriz Resultante:")
-	fmt.Println(m.ToStringB(7))*/
 }
 
-func pruebaMatriz() {
-	ma := g(7)
-	mb := matrizEntradaPrueba(7)
-	error,m := ma.Multiplicar(mb)
-	if error{
-		fmt.Println("Error al multiplicar")
-	}
-	
-	fmt.Println("G:")
-	fmt.Println(ma.ToString())
-	fmt.Println("Entrada:")
-	fmt.Println(mb.ToString())
-	fmt.Println("Matriz Resultante:")
-	fmt.Println(m.ToString())
-}
-
-func pruebaHGR(codificacion int,imprimir bool){
+func pruebaHGR(codificacion int) {
 	fmt.Println("H:")
-	h:=h(codificacion)
-	strinH:=h.ToString()
-	if imprimir {fmt.Println(strinH)}
+	h := h(codificacion)
+	h.ToFile("h2048.bin")
 
 	fmt.Println("G:")
-	g:=g(codificacion)
-	strinG:=g.ToString()
-	if imprimir {fmt.Println(strinG)}
-	
+	g := g(codificacion)
+	g.ToFile("g2048.bin")
+
 	fmt.Println("R:")
-	r:=r(codificacion)
-	strinR:=r.ToString()
-	if imprimir {fmt.Println(strinR)}
+	r := r(codificacion)
+	r.ToFile("r2048.bin")
 
-	
 	fmt.Println("Entrada:")
-	c:=matrizEntradaPrueba(codificacion)
-	strinC:=c.ToString()
-	if imprimir {fmt.Println(strinC)}
+	c := matrizEntradaPrueba(codificacion)
+	c.ToFile("entrada.bin")
 
-	error,codificada := g.Multiplicar(c)
+	error, codificada := g.Multiplicar(c)
 
-	AgregarError(&codificada)	
-	error,posicion:=TieneError(&codificada)
-	fmt.Println(fmt.Sprintf("Tienen errores:%v en %v",error,posicion))
+	AgregarError(&codificada)
+	error, posicion := TieneError(&codificada)
+	fmt.Println(fmt.Sprintf("Tienen errores:%v en %v", error, posicion))
 
 	CorregirError(&codificada)
-	error,posicion=TieneError(&codificada)
-	fmt.Println(fmt.Sprintf("Tienen errores:%v en %v",error,posicion))
+	error, posicion = TieneError(&codificada)
+	fmt.Println(fmt.Sprintf("Tienen errores:%v en %v", error, posicion))
 
 	fmt.Println("Corregida:")
-	if imprimir {fmt.Println(codificada.ToString())}
 
 	fmt.Println("Decodificada:")
-	error,decodificada := Decodificar(&codificada)
-	if !error{			
-		if imprimir {fmt.Println(decodificada.ToString())}
-	}else{
+	error, decodificada := Decodificar(&codificada)
+	if error {
 		fmt.Println("Error al multiplicar por R")
 	}
-	
+	decodificada.ToFile("decodificada.bin")
+
 }
 
-func pruebaHGR32(codificacion int,imprimir bool){
-	fmt.Println("H:")
-	h:=h(codificacion)
-	h32 := h.MatrizTo32()
-	strinH:=h32.ToString()
-	if imprimir {fmt.Println(strinH)}
-
-	fmt.Println("G:")
-	g:=g(codificacion)
-	g32:= g.MatrizTo32()
-	strinG:=g32.ToString()
-	if imprimir {fmt.Println(strinG)}
-	
-	fmt.Println("R:")
-	r:=r(codificacion)
-	r32 := r.MatrizTo32()
-	strinR:=r32.ToString()
-	if imprimir {fmt.Println(strinR)}
-
-	
-	fmt.Println("Entrada:")
-	c:=matrizEntradaPrueba(codificacion)
-	strinC:=c.ToString()
-	if imprimir {fmt.Println(strinC)}
-
-	error,codificada := g.Multiplicar(c)
-
-	AgregarError(&codificada)	
-	error,posicion:=TieneError(&codificada)
-	fmt.Println(fmt.Sprintf("Tienen errores:%v en %v",error,posicion))
-
-	CorregirError(&codificada)
-	error,posicion=TieneError(&codificada)
-	fmt.Println(fmt.Sprintf("Tienen errores:%v en %v",error,posicion))
-
-	fmt.Println("Corregida:")
-	if imprimir {fmt.Println(codificada.ToString())}
-
-	fmt.Println("Decodificada:")
-	error,decodificada := Decodificar(&codificada)
-	if !error{			
-		if imprimir {fmt.Println(decodificada.ToString())}
-	}else{
-		fmt.Println("Error al multiplicar por R")
-	}
-	
-}
-
-func esPotenciaDeDos( ent int)bool {
-	return ((ent != 0) && ((ent & (ent-1)) == 0))
+func esPotenciaDeDos(ent int) bool {
+	return ((ent != 0) && ((ent & (ent - 1)) == 0))
 }
 
 func bitsInformacion(ent int) int {
-	return(ent - (bitsParidad(ent)))
+	return (ent - (bitsParidad(ent)))
 }
 
-//bitsParidad Devuelve que candidad de bit corresponderian a bits de paridad para 
+//bitsParidad Devuelve que candidad de bit corresponderian a bits de paridad para
 //determinada codifciacion, si se desea para evitar iterar, podria hacerse un mapeo
 //con los 5 valores de codificacion que exiten.
-func bitsParidad(ent int) int{
-	n:=0
-	for i:=1;i<=ent;i=i*2{
-		n++;
+func bitsParidad(ent int) int {
+	n := 0
+	for i := 1; i <= ent; i = i * 2 {
+		n++
 	}
-	if(ent == 7){
-		return 3;}
-	if(ent == 31){
-		return 5;
+	if ent == 7 {
+		return 3
+	}
+	if ent == 31 {
+		return 5
 	}
 	return n
 }
+
 //h metodo que devuelve la matriz que se multiplica para codificar una entrada
 func h(codificacion int) Matriz {
 	alto := codificacion
-	ancho:=bitsParidad(codificacion)
-	aux := NuevaMatriz(ancho, alto )
-	for i:=0; i<ancho ;i++{
-		b:=1
-		uno:=false
-		for j:=0;j<alto;j++{
-			f:=float64(i)
-			if int(math.Pow(2,f))==b{
-				b=0
+	ancho := bitsParidad(codificacion)
+	aux := NuevaMatriz(ancho, alto)
+	for i := 0; i < ancho; i++ {
+		b := 1
+		uno := false
+		for j := 0; j < alto; j++ {
+			f := float64(i)
+			if int(math.Pow(2, f)) == b {
+				b = 0
 				uno = !uno
 			}
 			b++
-			if(uno){ 
-				aux.datos[i][j]=true
-			}       
+			if uno {
+				aux.datos[i][j] = true
+			}
 		}
 	}
 	return *aux
 }
 
-//g Funcion que crea la matriz generadora 
+//g Funcion que crea la matriz generadora
 func g(codificacion int) *Matriz {
-        n:=codificacion
-        m:=bitsInformacion(codificacion)
-        aux:=NuevaMatriz(n,m)
-        k:=0
-        p:=-1
-        for i:=0;i<n;i++{
-            if(!esPotenciaDeDos(i+1)){
-                aux.datos[i][k]=true
-                k++
-            }else{
-                    p++
-                    r:=1
-                    uno:=false
-                    b:=1;
-                    for j:=0;j<m; j++{
-                        for ;esPotenciaDeDos(r);{
-							r++;
-							f:=float64(p)
-                            if b== int(math.Pow(2,f)){
-                                uno=!uno
-                                b=0
-                            }
-                            b++
-						}
-						f:=float64(p)						
-                        if b == int( math.Pow(2,f)){
-                            uno=!uno
-                            b=0
-                        }
-                        b++
-                        if(uno){
-                            aux.datos[i][j]=true
-                        }
-                        r++                    
-                }
-            }
-        }
-        return aux
+	n := codificacion
+	m := bitsInformacion(codificacion)
+	aux := NuevaMatriz(n, m)
+	k := 0
+	p := -1
+	for i := 0; i < n; i++ {
+		if !esPotenciaDeDos(i + 1) {
+			aux.datos[i][k] = true
+			k++
+		} else {
+			p++
+			r := 1
+			uno := false
+			b := 1
+			for j := 0; j < m; j++ {
+				for esPotenciaDeDos(r) {
+					r++
+					f := float64(p)
+					if b == int(math.Pow(2, f)) {
+						uno = !uno
+						b = 0
+					}
+					b++
+				}
+				f := float64(p)
+				if b == int(math.Pow(2, f)) {
+					uno = !uno
+					b = 0
+				}
+				b++
+				if uno {
+					aux.datos[i][j] = true
+				}
+				r++
+			}
+		}
+	}
+	return aux
 }
-//r Funcion que crea la matriz decodificadora 
+
+//r Funcion que crea la matriz decodificadora
 func r(codificacion int) *Matriz {
-	n:=bitsInformacion(codificacion)
-	m:=codificacion
-	aux:=NuevaMatriz(n,m)
-	k:=0
-	for i:=0;i<m;i++{
-		if(!esPotenciaDeDos(i+1)){
-			aux.datos[k][i]=true
+	n := bitsInformacion(codificacion)
+	m := codificacion
+	aux := NuevaMatriz(n, m)
+	k := 0
+	for i := 0; i < m; i++ {
+		if !esPotenciaDeDos(i + 1) {
+			aux.datos[k][i] = true
 			k++
 		}
 	}
-	return aux;
+	return aux
 }
 
-
 //Codificar  Cifra el archivo de entrada
-func Codificar(operando *Matriz) (bool,Matriz){
+func Codificar(operando *Matriz) (bool, Matriz) {
 	cod := len(operando.datos)
-	h:=h(cod)
-	error,codificada :=h.Multiplicar(operando)
-	if error{
-		return true,*operando
+	h := h(cod)
+	error, codificada := h.Multiplicar(operando)
+	if error {
+		return true, *operando
 	}
-	return false,codificada
-	
+	return false, codificada
+}
+
+//Proteger funcion que toma de entrada el path de un archivo y lo codifica segun un valor de entrada
+func Proteger(url string, salida string, codificacion int) bool {
+	if _, err := os.Stat(url); os.IsNotExist(err) {
+		return false
+	}
+	file, err := os.Open(url)
+	manejoError(err)
+	defer file.Close()
+
+	fileO, err := os.OpenFile(salida, os.O_WRONLY, 0666)
+	manejoError(err)
+	defer fileO.Close()
+
+	bufferReader := bufio.NewReader(file)
+
+	buf := make([]byte, bitsInformacion(codificacion)/8)
+	h := h(codificacion)
+
+	byteLeidos, err := bufferReader.Read(buf)
+
+	manejoError(err)
+	for byteLeidos > 0 {
+		auxMatriz := MatrizColumna(ByteToBool(buf))
+		b, m := h.Multiplicar(auxMatriz)
+		if !b {
+			fileO.Write(m.ToString())
+		}
+
+	}
+
+	h.ToFile("h.bin")
+	return true
 }
 
 //TieneError verifica si tiene error una matriz, y devuelve la posicion del mismo.
-func TieneError(operando *Matriz) (bool, int){
-	codificacion:= len(operando.datos)
+func TieneError(operando *Matriz) (bool, int) {
+	codificacion := len(operando.datos)
 	h := h(codificacion)
-	error,sindrome := h.Multiplicar(operando)
+	error, sindrome := h.Multiplicar(operando)
 	if error {
 		fmt.Println("Error al multiplicar")
-		return true,-1
+		return true, -1
 	}
-	resultB:=sindrome.TieneUnos()
+	resultB := sindrome.TieneUnos()
 	resultI := -1
-	for i,fila:=range sindrome.datos{
-		for _,b := range fila{
-			if b{
+	for i, fila := range sindrome.datos {
+		for _, b := range fila {
+			if b {
 				f := float64(i)
-				resultI=resultI+int(math.Pow(2,f))
+				resultI = resultI + int(math.Pow(2, f))
 			}
 		}
 	}
 
- return resultB ,resultI
+	return resultB, resultI
 }
 
 //Decodificar descrifra el archivo de entrada
-func Decodificar(operando *Matriz)(bool,*Matriz) {
+func Decodificar(operando *Matriz) (bool, *Matriz) {
 	cod := len(operando.datos)
-	r:=r(cod)
-	error,decodificada :=r.Multiplicar(operando)
-	if error{
-		return true,operando
+	r := r(cod)
+	error, decodificada := r.Multiplicar(operando)
+	if error {
+		return true, operando
 	}
 	return false, &decodificada
 
 }
 
 //AgregarError a un archivo, si devuelve true es porque agrego error, sino ya habia antes un error
-func AgregarError(operando *Matriz) bool{
-	tieneError,_ :=TieneError(operando)
-	if tieneError{
+func AgregarError(operando *Matriz) bool {
+	tieneError, _ := TieneError(operando)
+	if tieneError {
 		return false
 	}
-	n:= len(operando.datos)
-	m:= len(operando.datos[0])
+	n := len(operando.datos)
+	m := len(operando.datos[0])
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	i := r.Intn(n)
 	j := r.Intn(m)
@@ -339,11 +275,11 @@ func AgregarError(operando *Matriz) bool{
 }
 
 //CorregirError toma como entrada una matriz, si la corrige retorna verdadero, sino retorna false
-func CorregirError(operando *Matriz)bool {
+func CorregirError(operando *Matriz) bool {
 	tieneError, posicion := TieneError(operando)
-	if tieneError{
-		if posicion !=-1{
-			operando.datos[posicion][0] = !operando.datos[posicion][0]	
+	if tieneError {
+		if posicion != -1 {
+			operando.datos[posicion][0] = !operando.datos[posicion][0]
 			return true
 		}
 	}
@@ -351,13 +287,13 @@ func CorregirError(operando *Matriz)bool {
 }
 
 //matrizEntradaPrueba genera una matriz de entrada aleatoria del tamaño de la codificacion
-func matrizEntradaPrueba (codificacion int) *Matriz{
-	aux := make([]bool ,bitsInformacion(codificacion))
+func matrizEntradaPrueba(codificacion int) *Matriz {
+	aux := make([]bool, bitsInformacion(codificacion))
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-	for i := range aux{
+	for i := range aux {
 		j := r.Intn(100)
-		aux[i]= (j%2 == 1)
+		aux[i] = (j%2 == 1)
 	}
-	m:=MatrizColumna(aux)
-	return  m
+	m := MatrizColumna(aux)
+	return m
 }
--- hamming/matriz.go
+++ hamming/matriz.go
@@ -1,8 +1,9 @@
 package hamming
 
 import (
-	"math"
 	"fmt"
+	"log"
+	"os"
 )
 
 //Matriz estructura para almacenar bits en matrices
@@ -10,14 +11,48 @@ type Matriz struct {
 	datos [][]bool
 }
 
-//Matriz32 estrutura para almacenar bits en enteros
-type Matriz32 struct{
-	datos [][]uint32
+//ToByte transforma una matriz en un arreglo de bytes
+func (operando *Matriz) ToByte() []byte {
+	ancho := len(operando.datos)
+	alto := len(operando.datos[0])
+	auxB := make([]byte, ancho*alto/8)
+	for indice, aux := range auxB {
+		//QUEDEACA
+	}
 }
+
 //ToString funcion que convierte una matriz en un string para imprimir en consola
 func (operando *Matriz) ToString() string {
 	var resultado string
-	fmt.Println(fmt.Sprintf("M: %v, N: %v", len(operando.datos),len(operando.datos[0])))
+	var aux uint64
+	var contador uint64
+	for _, r := range operando.datos {
+		for _, d := range r {
+			if d {
+				aux = aux | (1 << contador)
+			}
+			contador++
+			if contador%64 == 0 {
+				resultado = resultado + fmt.Sprintf("%v", aux)
+				aux = 0
+				contador = 0
+			}
+		}
+		if contador != 0 {
+			resultado = resultado + fmt.Sprintf("%v", aux)
+			aux = 0
+			contador = 0
+		}
+
+		//resultado = resultado + "\n"
+	}
+	return resultado
+}
+
+//ToStringConInfo funcion que convierte una matriz en un string para imprimir en consola
+func (operando *Matriz) ToStringConInfo() string {
+	var resultado string
+	fmt.Println(fmt.Sprintf("M: %v, N: %v", len(operando.datos), len(operando.datos[0])))
 	for _, r := range operando.datos {
 		resultado = resultado + "|"
 		for _, d := range r {
@@ -32,50 +67,16 @@ func (operando *Matriz) ToString() string {
 	return resultado
 }
 
-//ToString convierte una matriz en su representacion en String
-func (operando *Matriz32) ToString() string{
-	var resultado string
-	fmt.Println(fmt.Sprintf("M: %v, N: %v", len(operando.datos),len(operando.datos[0])))
-	for _, r := range operando.datos {
-		resultado = resultado + "|"
-		for _, d := range r {
-			resultado += fmt.Sprintf("%v", d)
-		}
-		resultado = resultado + "|\n"
-	}
-	return resultado
-}
-
-//ToStringB funcion que convierte una matriz en un string para imprimir en consola
-// donde como parametro toma el tamaño maximo de las filas
-func (operando *Matriz32) ToStringB(size int) string {
-	var resultado string
-	fmt.Println(fmt.Sprintf("M: %v, N: %v", len(operando.datos[0]),len(operando.datos)))
-	for _, r := range operando.datos {
-		resultado = resultado + "|"
-		for _, d := range r {
-			aux :=  fmt.Sprintf("%b",d)
-			for i:=len(aux); i <size; i ++ {
-				resultado+= "0"
-			}
-			resultado +=aux
-		}
-		resultado = resultado + "|\n"
-	}
-	return resultado
-}
-
-
 //Multiplicar Funcion que multiplica dos matrices y devuelve sus resultado
-func (operando *Matriz) Multiplicar(mE *Matriz)( bool,Matriz ){
-	if operando.datos != nil || operando.datos[0] != nil || mE.datos != nil || mE.datos[0] != nil {	
+func (operando *Matriz) Multiplicar(mE *Matriz) (bool, Matriz) {
+	if operando.datos != nil || operando.datos[0] != nil || mE.datos != nil || mE.datos[0] != nil {
 		nO := len(operando.datos[0])
 		mEn := len(mE.datos)
 
 		if nO == mEn {
 			m := len(operando.datos)
 			n := len(mE.datos[0])
-			aux := NuevaMatriz(m,n)
+			aux := NuevaMatriz(m, n)
 			for k := 0; k < m; k++ {
 				for j := 0; j < n; j++ {
 					for i := 0; i < nO; i++ {
@@ -83,58 +84,17 @@ func (operando *Matriz) Multiplicar(mE *Matriz)( bool,Matriz ){
 					}
 				}
 			}
-			return false,*aux
+			return false, *aux
 		}
 
-		fmt.Println("Error:El ancho y el alto de las matrices no concuerdan",nO,mEn)
+		fmt.Println("Error:El ancho y el alto de las matrices no concuerdan", nO, mEn)
 	} else {
 		fmt.Println("Error:no hay datos cargados en las matrices.")
 	}
 	c := [][]bool{{}}
 	var v = Matriz{datos: c}
-	return true,v
+	return true, v
 }
-
-//Mult Funcion que multiplica dos matrices y devuelve sus resultado
-func (operando *Matriz32) Mult(mE *Matriz32)( bool,Matriz32 ){
-	n := len(operando.datos[0])
-	p := len(mE.datos[0])
-/*
-	if nO == mEn {
-		m := len(operando.datos)
-		n := len(mE.datos)
-		aux := NuevaMatriz32(m,n)
-		for k := 0; k < m; k++ {
-			for j := 0; j < n; j++ {
-				for i := 0; i < nO; i++ {
-					aux.datos[k][j] = ((operando.datos[k][i] & mE.datos[i][j]) ^ aux.datos[k][j])
-				}
-			}
-		}
-		return false,*aux
-	}
-	*/
-	
-	if n == p {
-		m := len(operando.datos)
-		o := len(mE.datos)
-		aux := NuevaMatriz32(o,m)
-		for k := 0; k < o; k++ {
-			for j := 0; j < m; j++ {
-				for i := 0; i < n; i++ {
-					aux.datos[k][j] = ((operando.datos[k][j] & mE.datos[j][i]) ^ aux.datos[k][j])
-				}
-			}
-		}
-		return false,*aux
-	}
-	fmt.Println("Error:El ancho y el alto de las matrices no concuerdan")
-
-	c := [][]uint32{{}}
-	var v = Matriz32{datos: c}
-	return true,v
-}
-
 
 //TieneUnos controla si algun valor de la matriz es igual a 1
 func (operando *Matriz) TieneUnos() bool {
@@ -147,73 +107,49 @@ func (operando *Matriz) TieneUnos() bool {
 	}
 	return false
 }
-//TieneUnos controla si algun valor de la matriz es igual a 1
-func (operando *Matriz32) TieneUnos() bool {
-	for i := range operando.datos {
-		for j := range operando.datos[i] {
-			if operando.datos[i][j] != 0 {
-				return true
-			}
-		}
-	}
-	return false
-}
-
 
 //NuevaMatriz funcion que crea la matriz y le asigna espacio dato a su ancho x alto
-func NuevaMatriz(ancho int, alto int) *Matriz{
-	aux:= make([][]bool ,ancho)
-	for i:=range aux{
+func NuevaMatriz(ancho int, alto int) *Matriz {
+	aux := make([][]bool, ancho)
+	for i := range aux {
 		aux[i] = make([]bool, alto)
 	}
-	m:=Matriz{datos:aux}
+	m := Matriz{datos: aux}
 	return &m
 }
 
-//NuevaMatriz32 funcion que genera la matriz vacia de tal ancho y alto
-func NuevaMatriz32(ancho int,alto int) *Matriz32{
-	aux := make([][]uint32 , ancho)
-	for i:= range aux{
-		aux[i] = make([]uint32, alto)
-	}
-	m:= Matriz32{datos:aux}
-	return &m
-}
 //MatrizColumna apartir de una cadena de bytes crea la matriz columna
-func MatrizColumna(entrada []bool) *Matriz{
-	aux:= NuevaMatriz(len(entrada),1)
-	for i,b := range entrada{
+func MatrizColumna(entrada []bool) *Matriz {
+	aux := NuevaMatriz(len(entrada), 1)
+	for i, b := range entrada {
 		aux.datos[i][0] = b
 	}
 	return aux
 }
 
-//MatrizFila a partir de una cadena de enteros de 32 bits genera la matriz fila
-func MatrizFila(entrada []uint32) *Matriz32{
-	aux:= NuevaMatriz32(1,len(entrada))
-	for i,b := range entrada{
-		aux.datos[0][i] = b
+//ToFile graba una Matriz en un archivo binario
+func (entrada *Matriz) ToFile(url string) {
+	file, err := os.Create(url)
+	defer file.Close()
+	if err != nil {
+		log.Fatal(err)
 	}
-	return aux
+	file.WriteString(entrada.ToString())
+	return
 }
 
-
-//MatrizTo32 convierte una matriz de booleanos en una de 64 bits
-func (entrada *Matriz)MatrizTo32()*Matriz32{
-	ancho := len(entrada.datos[0])/64
-	if len(entrada.datos[0]) % 64 != 0{
-		ancho ++
+//ByteToBool convierte un arreglo de byte's en uno de booleanos
+func ByteToBool(entrada []byte) []bool {
+	auxB := make([]bool, len(entrada)*8)
+	for i, b := range entrada {
+		auxB[i*8] = ((b & 1) != 0)
+		auxB[i*8+1] = ((b & 2) != 0)
+		auxB[i*8+2] = ((b & 4) != 0)
+		auxB[i*8+3] = ((b & 8) != 0)
+		auxB[i*8+4] = ((b & 16) != 0)
+		auxB[i*8+5] = ((b & 32) != 0)
+		auxB[i*8+6] = ((b & 64) != 0)
+		auxB[i*8+7] = ((b & 128) != 0)
 	}
-	auxM := NuevaMatriz32(len(entrada.datos),ancho)
-
-	for indice,fila := range entrada.datos{
-		for indiceC, celda :=range fila{
-			if celda {
-				f :=math.Pow(2, float64(len(fila)-1-indiceC))
-				auxM.datos[indice][indiceC/64] = uint32(f) | auxM.datos[indice][indiceC/64]					
-			}	
-		}
-	}
-	return auxM
+	return auxB
 }
-
